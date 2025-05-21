4a84b49110215025bf7cc10fea749314e52cdfe3
elazzaouy-salaheddine <salaheddine.elazzaouy@outlook.com>
2025-05-21 22:52:49 +0100

    sffsf

--- core/settings.py
+++ core/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django.contrib.sites",
+    'django_cleanup',
     "allauth",
     "allauth.account",
     "allauth.socialaccount",
